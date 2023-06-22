4a15fb37eb746b182ff7813ee5c3cafeddfe2682
George Enciu <george.enciu@gmail.com>
2023-06-22 16:48:24 +0200

    test helpers for training events #1

--- libs/test-helpers/src/firestore/delete-collection.ts
+++ libs/test-helpers/src/firestore/delete-collection.ts
@@ -1,44 +1,15 @@
-import { getDatabaseCollection, getDatabaseClient } from '@rovacc/clients'
-
-export const deleteCollection1 = async (collection: string): Promise<void> => {
-  const collectionRef = getDatabaseCollection(collection)
-  const docs = await collectionRef.listDocuments()
-  const deletePromises = docs.map(async (doc) => await doc.delete())
-  console.log(deletePromises[0].then(console.log).catch(console.error));
-  await Promise.all(deletePromises)
-  console.log('=============')
-}
-
-
 export async function deleteCollection(collectionPath: string) {
-  const db = getDatabaseClient()
-  const collectionRef = getDatabaseCollection(collectionPath)
-  const query = collectionRef.orderBy('__name__').limit(100);
+  const endpoint = process.env['FIRESTORE_EMULATOR_HOST'] as string
+  const project = process.env['GCLOUD_PROJECT'] as string
 
-  return new Promise((resolve, reject) => {
-    deleteQueryBatch(db, query, resolve).catch(reject);
-  });
-}
-
-async function deleteQueryBatch(db, query, resolve) {
-  const snapshot = await query.get();
-
-  const batchSize = snapshot.size;
-  if (batchSize === 0) {
-    resolve();
-    return;
+  const response = await fetch(
+    `http://${endpoint}/emulator/v1/projects/${project}/databases/(default)/documents/${collectionPath}`,
+    {
+      method: 'DELETE',
+    }
+  );
+  if (response.status !== 200) {
+    throw new Error('Trouble clearing Emulator: ' + (await response.text()));
   }
-
-  // Delete documents in a batch
-  const batch = db.batch();
-  snapshot.docs.forEach((doc) => {
-    batch.delete(doc.ref);
-  });
-  await batch.commit();
-
-  // Recurse on the next process tick, to avoid
-  // exploding the stack.
-  process.nextTick(() => {
-    deleteQueryBatch(db, query, resolve);
-  });
 }
+
